4db3c946b186bdc071a44c81bb299d2d238c6aee
rupesh <rupesh@gmail.com>
2020-03-08 18:19:00 +0530

    second

--- example/views.py
+++ example/views.py
@@ -1,6 +1,5 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    return HttpResponse('Hello, world! this is our first view')
